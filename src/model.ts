ab3bae53227b02ef4080a8ace14ea1a4f8b41d53
BNK8991 <53058466+BNK8991@users.noreply.github.com>
2021-11-10 09:13:10 +0700

    commit

--- src/model.ts
+++ src/model.ts
@@ -2,7 +2,7 @@ export interface Property{
     id?: number,
     address: string,
     type: string,
-    bedroom: string,
+    bedroom: string, //turned out using number here is not wise since the database is not supported
     ListingDate: Date,
     rent: number,
     furniture: string,
